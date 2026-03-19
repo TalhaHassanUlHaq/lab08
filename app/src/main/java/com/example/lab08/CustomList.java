d9986e7565538724cabeb1c2167bda5006c69891
TalhaHassanUlHaq <27100306@lums.edu.pk>
2026-03-19 15:51:49 +0500

    3rd commit

--- app/src/main/java/com/example/lab08/CustomList.java
+++ app/src/main/java/com/example/lab08/CustomList.java
@@ -9,7 +9,9 @@ public class CustomList {
     public CustomList() {
         this.cities = new ArrayList<>();
     }
-
+    public boolean hasCity(City city) {
+        return cities.contains(city);
+    }
     public void addCity(City city) {
         cities.add(city);
     }
